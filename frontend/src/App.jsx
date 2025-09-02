704ff39f8ea26c9584262c777af4aa91d425f90c
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-09-02 11:08:22 +0100

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react"
+import {Routes,Route} from 'react-router-dom'
+
 const App = () => {
   return(
     <>
-      <div>
-        <h2 className="text-black">Hello</h2>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/collection' element={<Home/>} />
+          <Route path='/about' element={<Home/>} />
+          <Route path='/contact' element={<Home/>} />
+          <Route path='/card' element={<Home/>} />
+          <Route path='/login' element={<Home/>} />
+          <Route path='/place-order' element={<Home/>} />
+          <Route path='/product/:productId' element={<Home/>} />
+        </Routes>
       </div>
     </>
   )
