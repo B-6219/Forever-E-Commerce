06f5073cb2fd390706231666bb408dd6e3da68d7
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-09-02 11:26:25 +0100

    routing done✅

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,32 @@
-import React from "react"
-import {Routes,Route} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Orders from "./pages/Orders";
 
 const App = () => {
-  return(
-    <>
+  return (
+    <BrowserRouter>
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/collection' element={<Home/>} />
-          <Route path='/about' element={<Home/>} />
-          <Route path='/contact' element={<Home/>} />
-          <Route path='/card' element={<Home/>} />
-          <Route path='/login' element={<Home/>} />
-          <Route path='/place-order' element={<Home/>} />
-          <Route path='/product/:productId' element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/orders" element={<Orders />} />
         </Routes>
       </div>
-    </>
-  )
-}
+    </BrowserRouter>
+  );
+};
 
-
-export default App
+export default App;
