4e396baf505ea7fbf07abf5d323f57047ba6e55f
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-09-13 20:56:37 +0100

    starting with bestSeller.jsx

---
+++ frontend/src/components/BestSellers.jsx
@@ -0,0 +1,21 @@
+// import React, { useEffect, useState } from 'react'
+// import { ShopContext } from '../context/ShopContext'
+
+// // const BestSellers = () => {
+
+//    // const {products} = useContext(ShopContext)
+// //    const {bestSeller , setBestSeller} = useState( [] )
+
+// //    useEffect( () => {
+// //        console.log(products, bestSeller,setBestSeller);
+        
+// //    },{})
+
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default BestSellers
