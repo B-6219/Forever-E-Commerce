9f621e9ae5a53437a39846644f0e23315b6bfcb9
B-6219 <brayokama6219@gmail.com>
2025-09-28 10:26:09 +0300

    footer.......

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,26 +10,26 @@ import Product from "./pages/Product";
 import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import Collection from "./pages/Collection";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
 
-
-      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/collection" element={<Collection />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/place-order" element={<PlaceOrder />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/orders" element={<Orders />} />
-        </Routes>
-      </div>
-
+      <Footer />
+    </div>
   );
 };
 
