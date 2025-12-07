d9c82d96d04801bf0d7e898dc66d1a68055c7123
B-6219 <brayokama6219@gmail.com>
2025-12-07 11:36:23 +0300

    i found out that collection .jsx is long i will complete it later lemme focus on another  page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,13 +1,19 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {ShopContext} from '../context/ShopContext'
-import { assets } from '../assets/frontend_assets/assets'
+import { assets, products } from '../assets/frontend_assets/assets'
 import Title from '../components/Title'
+import ProductItem from '../components/ProductItem'
 
 
 const Collection = () => {
 
-  //const { products } = useContext(ShopContext)
+  const { products } = useContext(ShopContext)
   const [showFilter, setShowFilter] = useState(false)
+  const [filterProducts , setFilterProducts] = useState([])
+
+  useEffect( () => {
+    setFilterProducts(products)
+  } ,[])
 
   return (
 
@@ -68,14 +74,23 @@ const Collection = () => {
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={"ALL"} text2={"COLLECTIONS"}/>
           {/* Product Sort */}
-          <select classname="border-2 border-gray-300 text-sm px-2">
+          <select className="border-2 border-gray-300 text-sm px-2">
             <option value="relevant">Sort By: Relevant</option>
             <option value="low-high">Sort By: Low to High</option>
             <option value="high-low">Sort By: High to low</option>
           </select>
         </div>
 
-        
+        {/* Map Products */}
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+          {
+            filterProducts.map( (item , index) => {
+              <ProductItem key={index} name={item.name} id={item.id} price={item.price} image={item.image} />
+            })
+            
+          }
+        </div>
+
 
       </div>
 
