04110b1b57137f9e5813835bba057223541dd3f7
B-6219 <brayokama6219@gmail.com>
2025-12-07 12:25:23 +0300

    search bar is not showing on collection.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,7 @@
 import { createContext } from "react";
 
+
+
 // Default values (safe fallback if provider not used)
 export const ShopContext = createContext({
   products: [],
--- frontend/src/context/ShopContextProvider.jsx
+++ frontend/src/context/ShopContextProvider.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ShopContext } from "./ShopContext";
 import { products } from "../assets/frontend_assets/assets";
 
@@ -6,7 +6,13 @@ const ShopContextProvider = ({ children }) => {
   const currency = "Ksh";
   const delivery_Fee = 10;
 
-  const value = { products, currency, delivery_Fee };
+  const  [search , setSearch] = useState('')
+  const [showSearch , setShowSearch] = useState(false)
+
+  const value = { 
+    products, currency, delivery_Fee ,
+    search, setSearch ,showSearch , setShowSearch
+  };
 
   return (
     <ShopContext.Provider value={value}>
