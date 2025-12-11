d1b77169fe8a2a24d3bf846b28fe1f00d347c158
B-6219 <brayokama6219@gmail.com>
2025-12-11 16:15:40 +0300

    errors after errors😭😭

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,40 +1,41 @@
 import React, { useContext, useState } from 'react'
-import {assets } from '../assets/frontend_assets/assets'
+import { assets } from '../assets/frontend_assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
 import ShopContextProvider from '../context/ShopContextProvider'
 
 const Navbar = () => {
 
-  const [visible,setVisible] = useState(false)
+  const [visible, setVisible] = useState(false)
 
-  const [setShowSearch] = useContext(ShopContext)
+  //const [setShowSearch] = useContext(ShopContext)
+  const [setShowSearch] = useContext(ShopContextProvider)
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      
-      <Link to='/' > <img src={assets.logo} alt="" className='w-36'/> </Link>
+
+      <Link to='/' > <img src={assets.logo} alt="" className='w-36' /> </Link>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
         <NavLink to='./' className='flex flex-col items-center gap-1'>
-            <p>HOME</p>
-            <hr className='w-2/4 border-noneh-[1.5px] bg-gray-700 hidden'/>
+          <p>HOME</p>
+          <hr className='w-2/4 border-noneh-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
         <NavLink to='/about' className='flex flex-col items-center gap-1'>
-            <p>ABOUT</p>
-            <hr className='w-2/4 border-noneh-[1.5px] bg-gray-700 hidden'/>
+          <p>ABOUT</p>
+          <hr className='w-2/4 border-noneh-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
         <NavLink to='/collection' className='flex flex-col items-center gap-1'>
-            <p>COLLECTION</p>
-            <hr className='w-2/4 border-noneh-[1.5px] bg-gray-700 hidden'/>
+          <p>COLLECTION</p>
+          <hr className='w-2/4 border-noneh-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
         <NavLink to='/contact' className='flex flex-col items-center gap-1'>
-            <p>CONTACT</p>
-            <hr className='w-2/4 border-noneh-[1.5px] bg-gray-700 hidden'/>
+          <p>CONTACT</p>
+          <hr className='w-2/4 border-noneh-[1.5px] bg-gray-700 hidden' />
         </NavLink>
       </ul>
 
@@ -42,27 +43,27 @@ const Navbar = () => {
         <img onClick={() => setShowSearch()} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
         <div className='group relative'>
-            <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
-            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4 '>
-                <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-                    <p className='cursor-pointer hover:text-black'>My Profile</p>
-                    <p className='cursor-pointer hover:text-black'>Orders</p>
-                    <p className='cursor-pointer hover:text-black'>Logout</p>
-                </div>
+          <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+          <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4 '>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+              <p className='cursor-pointer hover:text-black'>My Profile</p>
+              <p className='cursor-pointer hover:text-black'>Orders</p>
+              <p className='cursor-pointer hover:text-black'>Logout</p>
             </div>
+          </div>
         </div>
 
         <Link to='/cart'>
-            <img src={assets.cart_icon} alt="" className='w-5 min-w-5'/>
-            <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+          <img src={assets.cart_icon} alt="" className='w-5 min-w-5' />
+          <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
-        <img onClick={()=> setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
+        <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
       </div>
 
       {/*Sidebar menu for small screens */}
-      <div className={`absolute top-0 bottom-0 right-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0' }`}>
+      <div className={`absolute top-0 bottom-0 right-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
         <div className='flex flex-col text-gray-600'>
-          <div onClick={()=> setVisible(false) } className='flex items-center gap-4 p-3 cursor-pointer'>
+          <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
             <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="" />
             <p className=''> Back</p>
           </div>
